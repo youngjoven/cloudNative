09cca618f25632610aaa4c2461368baf4d541222
youngjoven <rnjsdudwnd1@gmail.com>
2022-07-05 10:28:22 +0900

    Change mem

--- getmem/src/module/getmem.ts
+++ getmem/src/module/getmem.ts
@@ -4,7 +4,6 @@ import os from 'os';
 const router = express.Router();
 const meminfo = os.totalmem();
 router.get('/api/getmem', (req: Request, res: Response) => {
-    console.log(meminfo);
     res.send({meminfo});
 });
 
